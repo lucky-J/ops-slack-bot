5508603870141f5b857fe504dbcf13612b84ef98
p.maksimov <p.maksimov@keepsolid.com>
2018-12-17 20:59:29 +0200

    simple http for checking app via http

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+var (
+	listen = flag.String("listen", ":18080", "TCP address to listen to")
+)
 
 func main() {
-	fmt.Println("Hello, ops!")
+	flag.Parse()
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(*listen, nil))
+}
+
+func handler(writer http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(writer, "URI::%s", request.URL)
 }
