7534e4dc6cb5201e5086b649746ab67dd3122aae
p.maksimov <p.maksimov@keepsolid.com>
2018-12-17 22:16:04 +0200

    test slack bot functionality

--- main.go
+++ main.go
@@ -1,22 +1,47 @@
 package main
 
 import (
-	"flag"
+	"bytes"
+	"encoding/json"
 	"fmt"
-	"log"
+	"github.com/nlopes/slack"
+	"github.com/nlopes/slack/slackevents"
 	"net/http"
 )
 
-var (
-	listen = flag.String("listen", ":18080", "TCP address to listen to")
-)
+// here I provide app token from test chat
+// please contact @Pavel Maksymov if u want to join channel or use another token
+var api = slack.New("xoxb-463453827013-463161778324-pSVy6SbQBHQmQaKgTheryh01")
 
 func main() {
-	flag.Parse()
-	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(*listen, nil))
-}
+	// test events functionality
+	http.HandleFunc("/events-endpoint", func(w http.ResponseWriter, r *http.Request) {
+		buf := new(bytes.Buffer)
+		buf.ReadFrom(r.Body)
+		body := buf.String()
+		eventsAPIEvent, e := slackevents.ParseEvent(json.RawMessage(body), slackevents.OptionVerifyToken(&slackevents.TokenComparator{VerificationToken: "TOKEN"}))
+		if e != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Printf("%v", e)
+		}
 
-func handler(writer http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(writer, "URI::%s", request.URL)
+		if eventsAPIEvent.Type == slackevents.URLVerification {
+			var r *slackevents.ChallengeResponse
+			err := json.Unmarshal([]byte(body), &r)
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+			}
+			w.Header().Set("Content-Type", "text")
+			w.Write([]byte(r.Challenge))
+		}
+		if eventsAPIEvent.Type == slackevents.CallbackEvent {
+			innerEvent := eventsAPIEvent.InnerEvent
+			switch ev := innerEvent.Data.(type) {
+			case *slackevents.AppMentionEvent:
+				api.PostMessage(ev.Channel, slack.MsgOptionText("Yes, hello.", false))
+			}
+		}
+	})
+	fmt.Println("[INFO] Server listening")
+	http.ListenAndServe(":18080", nil)
 }
